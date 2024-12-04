ff36b0f2bfb1051702d11db4306e40cf009b8ffa
Sherly <sherlydrp@gmail.com>
2024-12-04 09:13:36 +0700

    Praktikum 8

--- laundryApps/src/confg/Database.java
+++ laundryApps/src/confg/Database.java
@@ -1,23 +1,40 @@
 package confg;
-import java.sql.Connection;  //import sqlnya
+
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
 public class Database {
-	Connection conn;
-	public static Connection koneksi () { //membuka method Connection untuk membuka koneksi ke database
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/laundry_apps", "root", "");
-		return conn;  //jika koneksi berhasil maka akan mengembalikan nilai connection
-		}
-		
-		catch (Exception e) {
-			JOptionPane.showInputDialog(null, e);
-			return null; //jika koneksi gagal maka akan tampil pesan eror menggunakan JOptionPane
-		}
-	}
-	
+    private static Database instance; // Singleton instance
+    private Connection connection;   // Connection untuk database
 
+    // Constructor private agar tidak bisa diakses dari luar class
+    private Database() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/laundry_apps", "root", "");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Koneksi database gagal: " + e.getMessage());
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    // Method untuk mendapatkan instance tunggal Database
+    public static Database getInstance() {
+        if (instance == null) {
+            synchronized (Database.class) { // Untuk menghindari masalah multithreading
+                if (instance == null) {
+                    instance = new Database();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // Method untuk mendapatkan koneksi
+    public Connection getConnection() {
+        return connection;
+    }
 }
--- laundryApps/src/service/LoginService.java
+++ laundryApps/src/service/LoginService.java
@@ -9,25 +9,22 @@ import confg.Database;
 import model.User;
 
 public class LoginService {
-	
-	public boolean authenticate(User user) {
-		String query = "SELECT * FROM user WHERE username = ? AND password =?";
-		
-		try (Connection conn = Database.koneksi();
-			PreparedStatement statement = conn.prepareStatement(query)) {
-			statement.setString(1, user.getUsername());
-			statement.setString(2, user.getPassword());
-					
-			ResultSet resultSet = statement.executeQuery();
-			return resultSet.next();
-				}
-		catch (SQLException e) {
-			e.printStackTrace();
-				}
-		
-		return false;
-				
-		
-	}
 
+    public boolean authenticate(User user) {
+        String query = "SELECT * FROM user WHERE username = ? AND password = ?";
+        
+        try (Connection conn = Database.getInstance().getConnection(); // Menggunakan Singleton
+             PreparedStatement statement = conn.prepareStatement(query)) {
+             
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getPassword());
+
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next(); // Jika ada hasil, berarti autentikasi berhasil
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Default return jika gagal
+    }
 }
